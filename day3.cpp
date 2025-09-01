a9e0d03ecb1ed64a2ea8390fafaa1a1700554aae
harshit-ag5965 <agarwalharshit5965@gmail.com>
2025-09-01 11:05:01 +0530

    day 3

--- day3.cpp
+++ day3.cpp
@@ -37,21 +37,55 @@ int main()
 
     // You are given marks of n groups of students in the format specified below. The strength of each group may not be the same. Print the maximum, minimum, total and average marks of each group.
 
+    // int t;
+    // cin>>t;
+    // for(int k=0;k<t;k++)
+    // {
+    //     int n;
+    //     cin>>n;
+    //     int arr[n];
+    //     int maxarr[n],minarr[n],sumarr[n],avgarr[n];
+        
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         cin>>arr[i];
+    //     }
+    //     cout<<*max_element(arr,arr+n)<<" "<<*min_element(arr,arr+n)<<" "<<accumulate(arr,arr+n,0)<<" "<<fixed<<setprecision(1)<<(float)accumulate(arr,arr+n,0)/n<<endl;
+    // }
+
+    // Read this input and print the sum of integers (if the elemts are integers) or print the length of longest word if the elemts are strings.
+
     int t;
     cin>>t;
-    for(int k=0;k<t;k++)
+    for(int i=0;i<t;i++)
     {
         int n;
-        cin>>n;
-        int arr[n];
-        int maxarr[n],minarr[n],sumarr[n],avgarr[n];
-        
-        for(int i=0;i<n;i++)
+        string type;
+        cin>>n>>type;
+        if(type =="STRING")
         {
-            cin>>arr[i];
+            string arr[n];
+            for(int j=0;j<n;j++)
+            {
+                cin>>arr[j];
+            }
+            int maxlen=0;
+            for(int j=0;j<n;j++)
+            {
+                maxlen=max(maxlen,(int)arr[j].length());
+            }
+            cout<<maxlen<<endl;
         }
-        cout<<*max_element(arr,arr+n)<<" "<<*min_element(arr,arr+n)<<" "<<accumulate(arr,arr+n,0)<<" "<<(float)accumulate(arr,arr+n,0)/n<<endl;
+        else if(type=="INT")
+        {
+            int arr[n];
+            for(int j=0;j<n;j++)
+            {
+                cin>>arr[j];
+            }
+            cout<<accumulate(arr,arr+n,0)<<endl;
+        }
+        
     }
-    
 
 }
